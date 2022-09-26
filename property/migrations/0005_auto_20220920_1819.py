9674279f686197d9acdcaa8abaedd2df27b31a68
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-09-26 11:47:39 +0300

    Отключите кэш в дата-миграции

--- property/migrations/0005_auto_20220920_1819.py
+++ property/migrations/0005_auto_20220920_1819.py
@@ -6,12 +6,19 @@ from django.db import migrations
 def transfer_owners(apps, schema_editor):
     Flats = apps.get_model('property', 'Flat')
     Owner = apps.get_model('property', 'Owner')
-    owners = Owner.objects.all()
-    for owner in owners:
-        owners_all_flats = Flats.objects.filter(owner=owner.Owner_name)
-        for flat in owners_all_flats:
-            owner.owner_apartments.add(flat)
-            owner.save()
+    for flat in Flats.objects.iterator():
+        for owner in Owner.objects.filter(name=flat.owner.iterator()):
+            owner.flat.add(flat)
+
+            # def transfer_owners(apps, schema_editor):
+            #     Flats = apps.get_model('property', 'Flat')
+            #     Owner = apps.get_model('property', 'Owner')
+            #     owners = Owner.objects.all()
+            #     for owner in owners:
+            #         owners_all_flats = Flats.objects.filter(owner=owner.Owner_name)
+            #         for flat in owners_all_flats:
+            #             owner.owner_apartments.add(flat)
+            #             owner.save()
 
 
 class Migration(migrations.Migration):
