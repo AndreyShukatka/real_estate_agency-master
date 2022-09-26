114c85cb8efebf0bc963769d0111821a336e41bd
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-09-26 11:49:20 +0300

    Update 0005_auto_20220920_1819.py

--- property/migrations/0005_auto_20220920_1819.py
+++ property/migrations/0005_auto_20220920_1819.py
@@ -10,16 +10,6 @@ def transfer_owners(apps, schema_editor):
         for owner in Owner.objects.filter(name=flat.owner.iterator()):
             owner.flat.add(flat)
 
-            # def transfer_owners(apps, schema_editor):
-            #     Flats = apps.get_model('property', 'Flat')
-            #     Owner = apps.get_model('property', 'Owner')
-            #     owners = Owner.objects.all()
-            #     for owner in owners:
-            #         owners_all_flats = Flats.objects.filter(owner=owner.Owner_name)
-            #         for flat in owners_all_flats:
-            #             owner.owner_apartments.add(flat)
-            #             owner.save()
-
 
 class Migration(migrations.Migration):
     dependencies = [
